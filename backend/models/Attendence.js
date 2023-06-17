bf0a6ca43730c245ec39bcd2ae91906c556bc858
abdullahsalman <abdullahsalmanmardan@gmail.com>
2023-06-17 21:27:48 +0500

    top bar minor changed

--- backend/models/Attendence.js
+++ backend/models/Attendence.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
 
-const attendenceSchema = new mongoose.Schema({
-  date: {
-    type: Date,
-    required: true,
+const attendenceSchema = new mongoose.Schema(
+  {
+    value: {
+      type: String,
+      required: true,
+    },
+    userid: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "userSchema",
+    },
   },
-  value: {
-    type: String,
-    required: true,
-  },
-  userid: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "userSchema",
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Attendence", attendenceSchema);
